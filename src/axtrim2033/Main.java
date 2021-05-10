86fa6d4b78aff4f758f138c96fd5c6a97005f78a
JulioDomingos7 <juliodomingos7@hotmail.com>
2021-05-10 20:41:59 -0300

    Incluindo meu desafio

--- src/axtrim2033/Main.java
+++ src/axtrim2033/Main.java
@@ -1,10 +1,87 @@
 package axtrim2033;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Teste");	
+
+		System.out.println("Teste");
 	}
 
+	public static void desafioJulio1() {
+
+		Scanner entrada = new Scanner(System.in);
+
+		boolean rcerta = false;
+		int compara = 0;
+
+		ArrayList alternativa = new ArrayList();
+		alternativa.add("(A)");
+		alternativa.add("(B)");
+		alternativa.add("(C)");
+		alternativa.add("(D)");
+		alternativa.add("(E)");
+
+		ArrayList resposta = new ArrayList();
+		resposta.add("NAND");
+		resposta.add("OR");
+		resposta.add("NO");
+		resposta.add("AND");
+		resposta.add("NOR");
+
+		String certa = "AND";
+
+		do {
+
+			System.out.println(
+					"Você precisa de uma porta lógica que verifique entradas e somente a partir desta verificação ");
+			System.out.println(
+					"se chegue à uma saída VERDADEIRA, no entanto, ambas entradas também precisam ser VERDADEIRAS.");
+			System.out.println("Qual porta lógica você deve usar para fazer essa verificação?");
+			for (int i = 1; i <= 5; i++) {
+				System.out.println(alternativa.get(i - 1) + " " + resposta.get(i - 1));
+			}
+			System.out.print("Escolha uma alternativa: ");
+			String escolha = entrada.next();
+			escolha = escolha.toLowerCase();
+
+			switch (escolha) {
+
+			case "a":
+				compara = 0;
+				break;
+
+			case "b":
+				compara = 1;
+				break;
+
+			case "c":
+				compara = 2;
+				break;
+
+			case "d":
+				compara = 3;
+				break;
+
+			case "e":
+				compara = 4;
+				break;
+
+			default:
+				System.out.println("Alternativa inválida!");
+			}
+			if (resposta.get(compara).equals(certa)) {
+				System.out.println("Resposta certa!");
+				rcerta = true;
+			} else {
+				System.out.println("Resposta incorreta, tente outra vez!");
+				Collections.shuffle(resposta);
+			}
+
+		} while (!rcerta);
+
+	}
 }
