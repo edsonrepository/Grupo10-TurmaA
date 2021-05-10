713db331f3af8e1c8dd31d41cb304cc53348987a
JulioDomingos7 <juliodomingos7@hotmail.com>
2021-05-10 20:45:42 -0300

    Criação do desafio 2

--- src/axtrim2033/Main.java
+++ src/axtrim2033/Main.java
@@ -84,4 +84,78 @@ public class Main {
 		} while (!rcerta);
 
 	}
+
+	public static void desafioJulio2() {
+
+		Scanner entrada = new Scanner(System.in);
+
+		boolean rcerta = false;
+		int compara = 0;
+
+		ArrayList alternativa = new ArrayList();
+		alternativa.add("(A)");
+		alternativa.add("(B)");
+		alternativa.add("(C)");
+		alternativa.add("(D)");
+		alternativa.add("(E)");
+
+		ArrayList resposta = new ArrayList();
+		resposta.add("NOR ; NOR ; NAND");
+		resposta.add("NAND ; NOR ; NOR");
+		resposta.add("NAND ; NAND ; NOR");
+		resposta.add("NOR ; NOR ; NOR");
+		resposta.add("NAND ; NAND ; NAND");
+
+		String certa = "NAND ; NAND ; NOR";
+
+		do {
+
+			System.out.println(
+					"Qual das alternativas representa a escolha adequada de portas lógicas em relação às seguintes necessidades:");
+			System.out.println("• Só tenha saída VERDADEIRA, quando as duas entradas forem FALSAS");
+			System.out.println("• Representa a lógica: S = ~ (A.B)");
+			System.out.println("• Representa a lógica: S = ~ (A+B)");
+			for (int i = 1; i <= 5; i++) {
+				System.out.println(alternativa.get(i - 1) + " " + resposta.get(i - 1));
+			}
+			System.out.print("Escolha uma alternativa: ");
+			String escolha = entrada.next();
+			escolha = escolha.toLowerCase();
+
+			switch (escolha) {
+
+			case "a":
+				compara = 0;
+				break;
+
+			case "b":
+				compara = 1;
+				break;
+
+			case "c":
+				compara = 2;
+				break;
+
+			case "d":
+				compara = 3;
+				break;
+
+			case "e":
+				compara = 4;
+				break;
+
+			default:
+				System.out.println("Alternativa inválida!");
+			}
+			if (resposta.get(compara).equals(certa)) {
+				System.out.println("Resposta certa!");
+				rcerta = true;
+			} else {
+				System.out.println("Resposta incorreta, tente outra vez!");
+				Collections.shuffle(resposta);
+			}
+
+		} while (!rcerta);
+
+	}
 }
